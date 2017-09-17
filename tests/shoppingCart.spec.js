bdd77d4112092df4c8f67773e231789422fbd801
Anny Chang <AnnyChang@kingstono365.onmicrosoft.com>
2017-09-17 10:50:43 +0800

    [紅燈] 取得條件參數設定 - 取得 VIP 設定值

--- tests/shoppingCart.spec.js
+++ tests/shoppingCart.spec.js
@@ -31,5 +31,22 @@ describe('ShoppingCart', () => {
             // Assert
             actual.should.deep.equal(expected);
         });
+
+        it('取得 VIP 設定值', () => {
+            // Arrange
+            let level = 'VIP';
+            let expected = {
+                atLeastPay: 500,
+                moq: 0,
+                discount: 0.8
+            };
+
+            // Act
+            let actual = shoppingCart.getLevelConfig(level);
+            
+            // Assert
+            actual.should.deep.equal(expected);
+        });
+
     });
 });
