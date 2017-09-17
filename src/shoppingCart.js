49fb0194680feedcf3b290d7a204edecb6eee7ef
Anny Chang <AnnyChang@kingstono365.onmicrosoft.com>
2017-09-17 10:04:34 +0800

    [紅燈] 取得條件參數設定 - 取得 Normal 設定值

---
+++ src/shoppingCart.js
@@ -0,0 +1,8 @@
+function ShoppingCart () {}
+
+// 條件參數設定值
+ShoppingCart.prototype.getLevelConfig = function (level) {
+    return {};
+};
+
+module.exports = ShoppingCart;
---
+++ tests/shoppingCart.spec.js
@@ -0,0 +1,35 @@
+let mocha = require('mocha');
+let chai = require('chai');
+let sinon = require('sinon');
+let ShoppingCart = require('../src/shoppingCart');
+
+// Use TDD should
+chai.should();
+
+describe('ShoppingCart', () => {
+    // 取得條件參數設定
+    describe('#getLevelConfig(level)', () => {
+
+        // 每次測試前重新 new ShoppingCart()
+        let shoppingCart;
+        beforeEach(() => {
+            shoppingCart = new ShoppingCart();
+        });
+
+        it('取得 Normal 設定值', () => {
+            // Arrange
+            let level = 'Normal';
+            let expected = {
+                atLeastPay: 1000,
+                moq: 3,
+                discount: 0.85
+            };
+
+            // Act
+            let actual = shoppingCart.getLevelConfig(level);
+            
+            // Assert
+            actual.should.deep.equal(expected);
+        });
+    });
+});
